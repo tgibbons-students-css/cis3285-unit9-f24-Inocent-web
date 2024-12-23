c74a3e1286091fd70f15c61427e5c85159057786
Inocent Maturure <imaturure@css.edu>
2024-12-22 23:30:58 -0800

    Learning Activity 9

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -11,11 +11,40 @@ namespace SingleResponsibilityPrinciple
     {
         public URLTradeDataProvider(string url, ILogger logger)
         {
+
+            string url;
+            ILogger logger;
+        public URLTradeDataProvider(string url, ILogger logger)
+        {
+            this.url = url;
+            this.logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    logger.LogWarning($"Failed to retrieve data. Status code: {response.StatusCode}");
+                    throw new Exception($"Error retrieving data from URL: {url}");
+                }
+
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+                }
+                return tradeData;
+            }
         }
     }
 }
